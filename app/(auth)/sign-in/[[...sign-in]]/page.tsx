edc648ea6ffe3021767ffc7b6a320f936fc38071
Priyanshu Gupta <priyanshu711403@gmail.com>
2024-11-12 03:21:09 +0530

    test user added

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -2,9 +2,18 @@ import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
    return (
-      <main className="auth-page">
-         <SignIn />
-      </main>
+      <div className="grid grid-cols-3">
+         <div className="border rounded-lg p-5 m-auto grid-span-1">
+            <p>
+               test username- :"test"
+               <br />
+               password-"test"
+            </p>
+         </div>
+         <main className="auth-page grid-span-2">
+            <SignIn />
+         </main>
+      </div>
    );
 };
 export default SignInPage;
